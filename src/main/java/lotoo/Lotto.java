24794b8bca404e97b5d4b31fe7b434fd6172c6f5
leah-codigo <leahmor001@gmail.com>
2026-03-29 20:38:34 +0900

    list > set 수정

--- src/main/java/lotoo/Lotto.java
+++ src/main/java/lotoo/Lotto.java
@@ -1,9 +1,6 @@
 package lotoo;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 public class Lotto {
     //1-45까지 로또번호 범위
@@ -15,27 +12,24 @@ public class Lotto {
     private static final int lottoNumberCount = 6;
     private static final int bonusNumber = 1 ;
 
-    private final List<Integer> numbers = new ArrayList<>();
+    private final Set<Integer> numbers = new HashSet<>();
     private final Random random = new Random();
 
     public void generateLottoNumbers() { // 6개 로또 번호 생성
-
-        for (int i = 0; i < lottoNumberCount; i++) {
+        while (numbers.size() < lottoNumberCount) {
             int num = random.nextInt(maxNumber)+minNumber;
-                while (numbers.contains(num)) {
-                    num = random.nextInt(maxNumber)+minNumber;
-                }
             numbers.add(num);
-            Collections.sort(numbers);
         }
+        Collections.sort(new ArrayList<>(numbers));
         System.out.println(numbers);
     }
 
     public void generateBonusNumber() { //보너스 번호 1개 생성
-            int bonus = random.nextInt(maxNumber)+minNumber;
-            while (numbers.contains(bonus)) {
-                bonus = random.nextInt(maxNumber)+minNumber;
-            }
+        int bonus = random.nextInt(maxNumber) + minNumber;
+        while (numbers.contains(bonus)) {
+            bonus = random.nextInt(maxNumber)+minNumber;
+        }
+        numbers.add(bonus);
         System.out.println(bonus);
     }
 }
--- src/main/java/lotoo/Lottos.java
+++ src/main/java/lotoo/Lottos.java
@@ -3,4 +3,7 @@ package lotoo;
 public class Lottos extends Lotto {
 
     //수량만큼 로또 생성
+
+
+
 }
